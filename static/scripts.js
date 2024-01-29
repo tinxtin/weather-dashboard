91422bfbb72ff63a30d2cd2db5fef74e31d3b370
Thinh <thinh1198@hotmail.com>
2024-01-29 11:46:46 +0000

    add dynamic history buttons

--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,10 @@
 var apiKey = "ad3a67673c70bf6e46cfdf36f8a1767d";
 
+function saveLocation(result) {
+    $('#history').append($('<button>').text(result).attr({id: 'history-btn', 'data-city': result}))
+    
+}
+
 async function getCoord(city) {
     queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
     return fetch(queryURL)
@@ -7,8 +12,8 @@ async function getCoord(city) {
             return response.json();
         })
         .then(function(data) {
-            console.log(data)
             return coord = {
+                city: data[0].name,
                 lat: data[0].lat,
                 lon: data[0].lon
             }
@@ -18,8 +23,8 @@ async function getCoord(city) {
 async function currWeather(city) {
     $('#today').empty();
     getCoord(city)
-    .then(function(response) {
-        queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${response.lat}&lon=${response.lon}&appid=${apiKey}&units=metric`;
+    .then(function(coord) {
+        queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}&units=metric`;
         fetch(queryURL)
             .then(function(response) {
                 return response.json();
@@ -31,7 +36,7 @@ async function currWeather(city) {
                 var icon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
                 var currDate = dayjs().format('D/M/YYYY');
                 $('#today').append(
-                    $('<h3>').text(`${data.name}`).append($('<img>').attr('src', icon)),
+                    $('<h3>').text(`${coord.city}`).append($('<img>').attr('src', icon)),
                     $('<div>').text(currDate),
                     $('<div>').text(`Temp: ${currTemp} °C`),
                     $('<div>').text(`Wind: ${currWind} MPH`),
@@ -41,17 +46,18 @@ async function currWeather(city) {
 }
 
 async function forecastWeather(city) {
+    $('#forecast').children('h4').empty();
+    $('#forecast').prepend($('<h4>').text('5-Day Forecast:'))
     $('#forecast-list').empty();
     var nextDate = 1;
     getCoord(city)
-    .then(function(response) {
-        queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${response.lat}&lon=${response.lon}&appid=${apiKey}&units=metric`;
+    .then(function(coord) {
+        queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}&units=metric`;
         fetch(queryURL)
             .then(function(response) {
                 return response.json();
             })
             .then(function(data) {
-                console.log(data)
                 for(var i = 7; i < data.cnt; i += 8) {
                     var temp = data.list[i].main.temp;
                     var wind = data.list[i].wind.speed;
@@ -72,11 +78,18 @@ async function forecastWeather(city) {
 
 $('#search-button').on('click', function(event) {
     event.preventDefault();
+    
     var result = $('#search-input').val();
     if (result === '') {return};
+    
     $('#search-input').val('');
-    currWeather(result)
-    forecastWeather(result)
+    currWeather(result);
+    forecastWeather(result);
+    saveLocation(result);
 })
 
+
+$(document).on('click', '#history-btn', function() {
+    console.log('test')
+})
 
